64cf6494a4474942217baa34475c7240191e8c30
Gustavo Corpus <guscorpus40@gmail.com>
2025-04-18 17:32:48 -0600

    Correccion de faltantes

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,14 @@
-const router = require("express").Router();
-const path = require("path");
-const fs = require("fs");
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
 
-router.get("/", (req, res) => {
-  const cardsPath = path.join(__dirname, "..", "data", "cards.json");
-  fs.readFile(cardsPath, { encoding: "utf8" }, (err, data) => {
+router.get('/', (req, res) => {
+  const cardsPath = path.join(__dirname, '..', 'data', 'cards.json');
+  fs.readFile(cardsPath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
-      return res.status(500).send({ message: "Error al leer el archivo" });
+      return res.status(500).send({ message: 'Ha ocurrido un error en el servidor' });
     }
-    res.send(JSON.parse(data));
+    return res.send(JSON.parse(data));
   });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,27 @@
-const router = require("express").Router();
-const path = require("path");
-const fs = require("fs");
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
 
-router.get("/", (req, res) => {
-  const usersPath = path.join(__dirname, "..", "data", "users.json");
-  fs.readFile(usersPath, { encoding: "utf8" }, (err, data) => {
+router.get('/', (req, res) => {
+  const usersPath = path.join(__dirname, '..', 'data', 'users.json');
+  fs.readFile(usersPath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
-      return res
-        .status(500)
-        .send({ message: "An error has occurred on the server" });
+      return res.status(500).send({ message: 'An error has occurred on the server' });
     }
-    res.send(JSON.parse(data));
+    return res.send(JSON.parse(data));
   });
 });
 
-router.get("/:id", (req, res) => {
-  const usersPath = path.join(__dirname, "..", "data", "users.json");
-  fs.readFile(usersPath, { encoding: "utf8" }, (err, data) => {
+router.get('/:id', (req, res) => {
+  const usersPath = path.join(__dirname, '..', 'data', 'users.json');
+  fs.readFile(usersPath, { encoding: 'utf8' }, (err, data) => {
     if (err) {
-      return res
-        .status(500)
-        .send({ message: "An error has occurred on the server" });
+      return res.status(500).send({ message: 'Ha ocurrido un error en el servidor' });
     }
     const users = JSON.parse(data);
     const user = users.find((u) => u._id === req.params.id);
     if (!user) {
-      return res.status(404).send({ message: "User ID not found" });
+      return res.status(404).send({ message: ' ID de usuario no encontrado' });
     }
     return res.send(user);
   });
