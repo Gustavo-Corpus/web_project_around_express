2c8d6a0a4f4a9d2671b172462f91f32664ba44c2
Gustavo Corpus <guscorpus40@gmail.com>
2025-04-26 19:47:07 -0600

    Primera iteración sprint 16

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/aroundb')
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error al conectar a MongoDB:', err);
+  });
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '680d5bdedd0eae00d7397ef5',
+  };
+  next();
+});
 
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
---
+++ controllers/users.js
@@ -0,0 +1,72 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ message: 'Error del servidor' }));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .orFail()
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'ID de usuario inválido' });
+      } else if (err.name === 'DocumentNotFoundError') {
+        res.status(404).send({ message: 'Usuario no encontrado' });
+      } else {
+        res.status(500).send({ message: 'Error del servidor' });
+      }
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Datos inválidos para crear usuario' });
+      } else {
+        res.status(500).send({ message: 'Error del servidor' });
+      }
+    });
+};
+
+module.exports.updateProfile = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Datos inválidos para actualizar el perfil' });
+      } else {
+        res.status(500).send({ message: 'Error del servidor' });
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'URL de avatar inválida' });
+      } else {
+        res.status(500).send({ message: 'Error del servidor' });
+      }
+    });
+};
